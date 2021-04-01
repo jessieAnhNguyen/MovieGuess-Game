198a8baba83c6d6078d2f547b29ff1d42dafdc6f
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2021-04-01 11:06:51 -0400

    initialized project and designed welcome fragment layout

---
+++ app/src/main/java/com/anguy39/movieguess/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.anguy39.movieguess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anguy39.movieguess.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
