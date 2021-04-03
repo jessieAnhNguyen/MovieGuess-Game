7bc4f31d0a06aa27eed56b2f794843417fdbd55c
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2021-04-03 14:16:45 -0400

    set up data binding for view model and game fragment

--- app/src/main/java/com/anguy39/movieguess/ui/main/game/GameFragment.kt
+++ app/src/main/java/com/anguy39/movieguess/ui/main/game/GameFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.navGraphViewModels
 import com.anguy39.movieguess.R
 import com.anguy39.movieguess.databinding.FragmentGameBinding
 
@@ -15,22 +19,26 @@ class GameFragment : Fragment() {
     private var _binding: FragmentGameBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var viewModel: GameViewModel
+    private val viewModel: GameViewModel by navGraphViewModels(R.id.nav_graph)
+
+    private var localAnswers = booleanArrayOf()
+    private var selection = -1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        _binding = FragmentGameBinding.inflate(inflater, container, false)
-        val view = binding.root
-        binding.gameOverButton.setOnClickListener {
-            view.findNavController().navigate(R.id.action_gameFragment_to_resultsFragment)
-        }
 
-        return view
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+        binding.run {
+            vm = viewModel
+            lifecycleOwner = this@GameFragment
+        }
+        
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
         // TODO: Use the ViewModel
     }
 
