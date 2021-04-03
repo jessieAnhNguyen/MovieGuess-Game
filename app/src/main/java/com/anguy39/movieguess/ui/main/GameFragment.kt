84407750790b904ebb39b82e3c09fce07f82a86f
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2021-04-03 12:40:20 -0400

    added nav between game fragment and result fragment

--- app/src/main/java/com/anguy39/movieguess/ui/main/GameFragment.kt
+++ app/src/main/java/com/anguy39/movieguess/ui/main/GameFragment.kt
@@ -6,25 +6,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.anguy39.movieguess.R
+import com.anguy39.movieguess.databinding.FragmentConfigBinding
+import com.anguy39.movieguess.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GameFragment()
-    }
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!
 
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        val view = binding.root
+        binding.gameOverButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_gameFragment_to_resultsFragment)
+        }
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
